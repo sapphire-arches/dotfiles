421372a103e2aebf390432f984ec18495b34352f
bobtwinkles <srkoser+GitHub@gmail.com>
2013-11-19 18:44:31 -0500

    I can totaly shell script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 wd=$(pwd)
-ln -s $(HOME)/.vimrc    $(wd)/vimrc
-ln -s $(HOME)/.vim      $(wd)/vim
-ln -s $(HOME)/.mostrc   $(wd)/mostrc
-ln -s $(HOME)/.xmonad   $(wd)/xmonad
+ln -s HOME/.vimrc    wd/vimrc
+ln -s HOME/.vim      wd/vim
+ln -s HOME/.mostrc   wd/mostrc
+ln -s HOME/.xmonad   wd/xmonad
