aa10a11407c991c363d947161ad952436538a3cb
bobtwinkles <srkoser+GitHub@gmail.com>
2013-12-24 22:50:11 -0500

    Fix volume

--- xmonad/volume.sh
+++ xmonad/volume.sh
@@ -1,3 +1,3 @@
-volume_percent=$(amixer | grep -A 5 Master | grep "Mono:" | cut -d " " -f 6 | sed "s/\[//" | sed "s/\]//")
+volume_percent=$(amixer | grep -A 5 Master | grep "Front Left:" | cut -d " " -f 7 | sed "s/\[//" | sed "s/\]//")
 #strip leading spaces
 echo -n "Volume:" $volume_percent
