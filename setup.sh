a07d74d2ea3211de5a40dfdae118fc3adc78ed99
bobtwinkles <srkoser+GitHub@gmail.com>
2014-01-10 23:14:53 -0500

    #ZSH MASTER RACE

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 wd=$(pwd)
-ln -s $wd/vimrc     $HOME/.vimrc
-ln -s $wd/vim       $HOME/.vim
-ln -s $wd/mostrc    $HOME/.mostrc
-ln -s $wd/xmonad    $HOME/.xmonad
-ln -s $wd/bashrc    $HOME/.bashrc
+ln -s $wd/vimrc       $HOME/.vimrc
+ln -s $wd/vim         $HOME/.vim
+ln -s $wd/mostrc      $HOME/.mostrc
+ln -s $wd/xmonad      $HOME/.xmonad
+ln -s $wd/bashrc      $HOME/.bashrc
+ln -s #wd/zshrc       $HOME/.zshrc
+ln -s #wd/zshrc.local $HOME/.zshrc.local
