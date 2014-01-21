f7d79fc36c8c6ecc063093ee5e485c51d396cf86
bobtwinkles <srkoser+GitHub@gmail.com>
2014-01-20 21:32:25 -0500

    Add custom print stuffs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+import XMonad.Core
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -8,15 +9,20 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.WorkspaceCompare
 import XMonad.Util.Loggers
--- import XMoand.Util.NamedWindows (getName)
+import XMonad.Util.NamedWindows (getName)
 import XMonad.Layout.Grid
 import XMonad.Layout.NoBorders
 import XMonad.Layout.SimplestFloat
+import XMonad.Layout.OneBig
+import XMonad.Layout.Simplest
 import System.IO
 import Data.List
-import Data.Traversable (traverse)
+import Data.Maybe
+import Data.Traversable (traverse, fmapDefault)
 import qualified XMonad.StackSet as W
+import Control.Monad.State.Lazy
 
+-- Starts an xmobar on a specified screen
 xmobar screen config = spawnPipe . intercalate " " $ options
     where options = [ "xmobar"
                      , "-x"
@@ -24,6 +30,7 @@ xmobar screen config = spawnPipe . intercalate " " $ options
                      , show config
                      ]
 
+-- Autofloat some special windows, put things in their place
 myManageHook = composeAll
     [ title =? "Fireworks"          --> doFloat
     , title =? "Isomtric Renderer"  --> doFloat
@@ -37,6 +44,7 @@ myManageHook = composeAll
     , title =? "Phys Canvas"        --> doFloat
     , title =? "Eclipse"            --> doFloat
     , title =? "testing"            --> doFloat
+    , title =? "xmessage"           --> doFloat
     , title =? "Defend Rome"        --> doFullFloat
     , title =? "Team Fortress 2 - OpenGL" --> doFullFloat
     , title =? "Nightly"            --> doShift "3"
@@ -48,15 +56,44 @@ myManageHook = composeAll
     , manageDocks
     ]
 
-myLayoutHook = noBorders $ avoidStrutsOn [D] $ (Full ||| simplestFloat ||| tiled ||| Mirror tiled) where
+-- Custom layout stuff
+myLayoutHook = noBorders $ avoidStrutsOn [D] $ (Simplest ||| Full ||| simplestFloat ||| tiled ||| Mirror tiled ||| OneBig (3/4) (3/4)) where
                tiled = Tall nmaster delta ratio
                nmaster = 1
                delta = 3/100
                ratio = 1/2
 
---myTitleLogger :: X (Maybe String)
---myTitleLogger = withWindowSet $ traverse (fmap show . getName) . (take 20 W.peek)
+-- XMobar output stuff
+myTitleLength = 30
 
+formatTitle :: Int -> String -> String
+formatTitle maxLength t
+    | len <  maxLength = formatTitle maxLength ( t ++ " " )
+    | len == maxLength = t
+    | len >  maxLength = shorten maxLength t
+    where len = length t;
+
+formatTitles :: Int -> [String] -> String
+formatTitles maxLength ( x : xs ) = "[" ++ (formatTitle maxLength x) ++ "]" ++ formatTitles maxLength xs
+formatTitles maxLength _          = ""
+
+listWindowTitles :: [Window] -> X [String]
+listWindowTitles xs = traverse (fmap show . getName) xs
+
+logTitles :: X ( Maybe String )
+logTitles = withWindowSet $ fmap Just . (\x ->
+          let 
+            focused       = W.peek x
+            allWindows    = W.index x
+            windows       = case focused of
+                                 Nothing -> []
+                                 Just x  -> filter (\y -> not ( Just y == focused ) ) allWindows
+            numWindows    = length windows
+            titles        = listWindowTitles windows
+            desiredLength = min (quot 50 numWindows) myTitleLength
+          in fmap (formatTitles desiredLength) titles)
+
+-- And the main config
 main = do 
     xmproc <- Main.xmobar 0 ".xmonad/xmobarrc"
     xmonad $ ewmh defaultConfig
@@ -66,11 +103,12 @@ main = do
         , startupHook = ewmhDesktopsStartup >> setWMName "LG3D"
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "#859900" "" . shorten 100
+                        , ppTitle = wrap "[" "]" . xmobarColor "#859900" "" . formatTitle myTitleLength
+                        , ppSep = "|"
                         , ppHidden = xmobarColor "#b58900" ""
                         , ppCurrent = wrap "[" "]" . xmobarColor "#dc322f" "" 
                         , ppHiddenNoWindows = xmobarColor "#93a1a1" ""
---                        , ppExtras = [logTitle]
+                        , ppExtras = [ logTitles ]
                         }
         , borderWidth = 2
         , normalBorderColor  = "#268bd2"
