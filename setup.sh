81ee26b420365ebfdbfa46b9f2ad03478354df48
bobtwinkles <srkoser+GitHub@gmail.com>
2014-09-23 18:00:44 -0400

    Put custom theme where it should be (and as a side effect, allow us to easilly update oh-my-zsh)

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 wd=$(pwd)
+mkdir -p $wd/oh-my-zsh/custom/themes
 ln -s $wd/vimrc            $HOME/.vimrc
 ln -s $wd/vim              $HOME/.vim
 ln -s $wd/mostrc           $HOME/.mostrc
@@ -7,3 +8,4 @@ ln -s $wd/bashrc           $HOME/.bashrc
 ln -s $wd/zshrc            $HOME/.zshrc
 ln -s $wd/oh-my-zsh        $HOME/.oh-my-zsh
 ln -s $wd/shell_config.sh  $HOME/.shell_config.sh
+ln -s $wd/bob_twinkles.zsh-theme $HOME/oh-my-zsh/custom/themes
