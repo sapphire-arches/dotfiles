f3e8707c403fbad625a8eaf8e4913c549c6b4362
bobtwinkles <srkoser+github@gmail.com>
2018-03-14 17:14:38 -0400

    Add fcitx support

--- xmonad/startup.sh
+++ xmonad/startup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS="@im=fcitx"
+
+fcitx &
+xmodmap ~/.xmodmap
 xrdb -merge ~/.Xresources
 xscreensaver -no-splash &
 xcompmgr &
