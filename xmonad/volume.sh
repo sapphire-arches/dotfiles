c9b623ef9ce4d83a2636f57f5143c902fd4339e6
bobtwinkles <srkoser+github@gmail.com>
2020-03-25 12:04:03 -0700

    Use bin/env for xmonad bash scripts

--- xmonad/volume.sh
+++ xmonad/volume.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 volume_percent=$(amixer -D pulse sget Master | grep "Front Left:" | cut -d " " -f 7 | sed "s/\[//" | sed "s/%\]//")
 #strip leading spaces
 echo -n "Volume:" $volume_percent
