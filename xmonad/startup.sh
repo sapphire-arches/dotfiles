7e295fc05aa817884dd9526b238f18bb5ebda02c
bobtwinkles <srkoser+github@gmail.com>
2021-12-12 22:24:44 -0800

    carrot-cake monitor setup

--- xmonad/startup.sh
+++ xmonad/startup.sh
@@ -4,6 +4,12 @@ export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 export XMODIFIERS="@im=fcitx"
 
+if [[ $(hostname) -eq "carrot-cake" ]]
+then
+  # carrot-cake only xrandr setup
+  xrandr --output HDMI-0 --left-of DP-1
+fi
+
 fcitx &
 xmodmap ~/.xmodmap
 xrdb -merge ~/.Xresources
